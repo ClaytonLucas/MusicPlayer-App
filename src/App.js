4656768b22901679c9f987c97890c49b8a1f0013
Lucas Lima <clayton.lucas07@gmail.com>
2021-01-08 01:51:53 -0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import Player from './components/Player';
 
 function App() {
+  const [songs] = useState([
+    {
+      title: "Change The World",
+      artist: "Eric Clapton",
+      img_src: "./images/song-1.jpg",
+      src: "./music/eric clapton change the world.mp3"
+    },
+    {
+      title: "Head Over Heels",
+      artist: "Tears or Fears",
+      img_src: "./images/song-2.jpg",
+      src: "./music/tears for fears head over heels.mp3"
+    },
+    {
+      title: "You Get What You Give",
+      artist: "New Radicals",
+      img_src: "./images/song-3.jpg",
+      src: "./music/new radicals you get what you give.mp3"
+    },
+    {
+      title: "Virtual Insanity",
+      artist: "Jamiroquai",
+      img_src: "./images/song-4.jpg",
+      src: "./music/Jamiroquai - Virtual Insanity.mp3"
+    }
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+  
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player 
+      currentSongIndex={currentSongIndex} 
+      setCurrentSongIndex={setCurrentSongIndex}
+      nextSongIndex={nextSongIndex} 
+      songs={songs}></Player>
     </div>
   );
 }
